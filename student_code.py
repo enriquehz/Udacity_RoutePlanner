6e022399a8daf76566c3bde5be39863290971451
Enrique Hernandez <enrique.hz.t@gmail.com>
2018-01-09 21:08:19 -0600

    Second_Udacity_Submit
    Second submit for revision

--- student_code.py
+++ student_code.py
@@ -1,118 +1,110 @@
+#I've based my code in the wikipedia A* pseudocode: 
+# https://en.wikipedia.org/wiki/A*_search_algorithm
+
+
 import math
-from helpers import show_map
 
-def shortest_path(M,start,goal):
+def shortest_path(M, start, goal):
     
-    set_explored = set()
-    final_path = []
-    heuristic_values = []
-    frontier_values = []
-    distances = {}
+    points = M.intersections 
+    #print(points)
+    roads = M.roads
+    #print(roads)
     
-    print("shortest path called")
+    closed_set = set()
+    opened_set = set([start])
+    node_number = len(points)
     
-    final_path.append(start)
-    set_explored.add(start)
+    came_from = {}
     
+    g_Score = []
+    f_Score = []
     
-    ########## MY TESTS ##########
+    for nodes in range(node_number):
+        g_Score.append(999)
+        f_Score.append(999)
+        
+    g_Score[start] = 0.0    
+    f_Score[start] = distance_between_points(start, goal, points)
     
-    #show_map(M, start, goal)
+    connected_nodes = roads[start]
     
-    #print (distance_between_points(M.intersections[start],M.intersections[goal]))
+    while (len(opened_set) != 0 ):
+        
+        current_node = find_lowest_f (opened_set, f_Score) 
+        
+        if (current_node == goal):
+            print ("Path Finded")
+            
+            print(reconstruct_path(came_from, current_node))
+            return reconstruct_path(came_from, current_node)
+            
+        opened_set.remove(current_node)
+        closed_set.add(current_node)
+        
+        neighbors_nodes = roads[current_node]
+        
+        for neighbor in neighbors_nodes:
+            if neighbor in closed_set:
+                continue
+            
+            if neighbor not in opened_set:
+                opened_set.add(neighbor)
+            
+            tentative_g_Score = g_Score[current_node] + distance_between_points(current_node, neighbor, points)
     
-    heuristic_values = get_heuristic_value(M, goal)
-    #print (heuristic_values)
-    
-    frontier_values = get_frontier_intersections(start , M.roads)
-    distances = adjacent_distances(M.intersections[start], frontier_values, M)
-    #print (frontier_values)
-    #print (distances)
-    
-    #print(calculate_h_function (distances, frontier_values, heuristic_values))
-    
-    
-    next_intersection = calculate_h_function (distances, frontier_values, heuristic_values)
+            if tentative_g_Score >= g_Score[neighbor]:
+                continue
+            
+            came_from[neighbor] = current_node
+            g_Score[neighbor] = tentative_g_Score
+            f_Score[neighbor] = g_Score[neighbor] + distance_between_points(neighbor, goal, points)
 
-    final_path.append(next_intersection)
-    ############################
+    return None
+
+
+
+    #print (g_Score)
+    #print (f_Score)
     
-    if (next_intersection not in set_explored):
     
-        while (goal not in final_path):
-
-            frontier_values = get_frontier_intersections(next_intersection , M.roads)
-            distances = adjacent_distances(M.intersections[next_intersection], frontier_values, M)
-
-            next_intersection = calculate_h_function (distances, frontier_values, heuristic_values)
-
-            final_path.append(next_intersection)
-
-        print(final_path)
-    return final_path
-
-
-def distance_between_points(point_1, point_2):
+def reconstruct_path(came_from, current_node):
+    total_path = [current_node]
     
-    distance_value = 0
+    while (current_node in came_from.keys()):
+        current_node = came_from[current_node]
+        total_path.append(current_node)
     
-    distance_value = math.sqrt( (point_2[1] - point_1[1]) ** 2  + (point_2[0] - point_1[0]) ** 2)
+    reversed_total_path = total_path[::-1]
+    
+    return reversed_total_path
+    
+    
+    
+def find_lowest_f(opened_set, f_Score):
+
+    next_node = 0
+    new_acum = 99999
+    
+    for node in opened_set:
+        acum = f_Score[node] 
+        if (new_acum > acum):
+            new_acum = acum
+            next_node = node
+           
+    return next_node
+
+
+
+def distance_between_points(start, goal, points):
+    x1, y1 = points[start]
+    #print("x1: ", x1)
+    #print("y1: ", y1)
+    x2, y2 = points[goal]
+    #print("x2: ", x2)
+    #print("y2:", y2)    
+    
+    distance_value = math.sqrt( (x2 - x1) ** 2  + (y2 - y1) ** 2)
     
     return distance_value
 
-
-# get the value of h (f = g + h) 
-
-def get_heuristic_value (M, goal):
-    
-    heuristic_values = []
-    
-    for values in M.intersections:
-        heuristic_values.append(distance_between_points(M.intersections[values],M.intersections[goal]))
-    
-    return heuristic_values
-
-
-def get_frontier_intersections (current_intersection, roads):
-    
-    frontier_values = []
-    
-    frontier_values = roads[current_intersection]
-    
-    return frontier_values
-
-
-# get the value of g (f = g + h) 
-
-def adjacent_distances (current_intersection, frontier_values, M):
-    
-    adj_distance = {}
-    
-    for key in frontier_values:
-        adj_distance[key] = distance_between_points(current_intersection, M.intersections[key])
-        
-    return adj_distance
-       
-    
-def calculate_h_function (adjacent_distance, frontier_intersections, heuristic_values):
-    
-    shortest_path = []
-    new_acum = 99
-    
-    for key in adjacent_distance:
-        acum = heuristic_values[key] + adjacent_distance[key]
-        #print(acum)
-        if (new_acum > acum):
-            new_acum = acum
-            key_value = key
-            #set_explored.add(key_value)
- 
-    #print(key_value)
-    #print (new_acum)    
-    return key_value
-        
-     
-        
-    
-    
-    
